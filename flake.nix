a148fa6dfb82a6943b8ae0be2a061422e2f55071
rpgwaiter <rpgwaiter@based.zone>
2025-06-25 04:57:38 +0000

    update nix flake, it's not building tho :(

--- flake.nix
+++ flake.nix
@@ -5,87 +5,91 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs =
-    inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = import inputs.systems;
-      perSystem =
-        {
-          pkgs,
-          ...
-        }:
-        let
-          cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-          rust-toolchain = pkgs.symlinkJoin {
-            name = "rust-toolchain";
-            paths = with pkgs; [
-              rustc
-              cargo
-              cargo-watch
-              rust-analyzer
-              rustPlatform.rustcSrc
-              cargo-dist
-              cargo-tarpaulin
-              cargo-insta
-              cargo-machete
-              cargo-edit
-            ];
-          };
-
-          buildInputs = with pkgs; [
-            at-spi2-atk
-            atkmm
-            cairo
-            gdk-pixbuf
-            glib
-            gtk3
-            harfbuzz
-            librsvg
-            libsoup_3
-            pango
-            webkitgtk_4_1
-            openssl
-            wasm-bindgen-cli
-            lld_20
-          ];
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            gobject-introspection
+      perSystem = {pkgs, ...}: let
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        rust-toolchain = pkgs.symlinkJoin {
+          name = "rust-toolchain";
+          paths = with pkgs; [
+            rustc
             cargo
-            cargo-tauri
-            nodejs
+            cargo-watch
+            rust-analyzer
+            rustPlatform.rustcSrc
+            cargo-dist
+            cargo-tarpaulin
+            cargo-insta
+            cargo-machete
+            cargo-edit
+            wasm-bindgen-cli_0_2_100
           ];
-        in
-        {
-          # Rust package
-          packages.default = pkgs.rustPlatform.buildRustPackage {
-            inherit (cargoToml.package) name version;
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
-
-            RUST_BACKTRACE = "full";
-
-            nativeBuildInputs = nativeBuildInputs;
-            buildInputs = buildInputs;
-          };
-
-          # Rust dev environment
-          devShells.default = pkgs.mkShell {
-            RUST_BACKTRACE = "full";
-            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
-
-            packages =
-              nativeBuildInputs
-              ++ buildInputs
-              ++ [
-                rust-toolchain
-              ]
-              ++ (with pkgs; [
-                clippy
-                dioxus-cli
-              ]);
-          };
-
         };
+
+        buildInputs = with pkgs; [
+          at-spi2-atk
+          atkmm
+          cairo
+          gdk-pixbuf
+          glib
+          gtk3
+          harfbuzz
+          librsvg
+          libsoup_3
+          pango
+          webkitgtk_4_1
+          openssl
+          wasm-bindgen-cli_0_2_100
+          lld_20
+          dioxus-cli
+        ];
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          gobject-introspection
+          cargo
+          cargo-tauri
+          wasm-bindgen-cli_0_2_100
+          nodejs
+        ];
+      in {
+        # Rust package
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          inherit (cargoToml.package) name version;
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          buildPhase = ''
+            ${pkgs.dioxus-cli}/bin/dx build --platform=web --features=web --release
+          '';
+
+          installPhase = ''
+            mkdir $out/share
+            mv ./target/dx/basedradio-rs/release/web/public $out/share/
+          '';
+
+          RUST_BACKTRACE = "full";
+
+          nativeBuildInputs = nativeBuildInputs;
+          buildInputs = buildInputs;
+        };
+
+        # Rust dev environment
+        devShells.default = pkgs.mkShell {
+          RUST_BACKTRACE = "full";
+          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+
+          packages =
+            nativeBuildInputs
+            ++ buildInputs
+            ++ [
+              rust-toolchain
+            ]
+            ++ (with pkgs; [
+              clippy
+              dioxus-cli
+            ]);
+        };
+      };
     };
 }
