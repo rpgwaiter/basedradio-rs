52776372b35ed1a554adf9a14f504192d567cd20
rpgwaiter <rpgwaiter@based.zone>
2026-07-25 07:14:34 +0000

    final github commit

--- app/assets/wasm-check.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    if (typeof WebAssembly === "object" && typeof WebAssembly.instantiate === "function") {
-      return;
-    } else {
-      window.location.href = "https://old.based.radio";
-    }
-  })();
